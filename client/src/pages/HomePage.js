f5fe07ea591845e10fdf253f1a9ca2a92bfedaba
Okkes <okkesakkurt46@gmail.com>
2023-09-17 22:11:00 +0300

    second carousel and card completed

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SuperPriceOffer from '../components/home/SuperPriceOffer'
+import AdCards from '../components/home/AdCards'
+
+const HomePage = () => {
+  return (
+      <div>
+      <SuperPriceOffer />
+      <AdCards/>
+    </div>
+  )
+}
+
+export default HomePage
